827f04578bf20b922929de7d38637d183cff6315
Sandra <sandramoroz@yandex.ru>
2019-09-12 13:51:50 +0300

    step 3

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+import { greeting } from '..';
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+greeting(name);
