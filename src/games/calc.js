2d0441ab5955a6ebc77df5977638484b6d8ce75d
Sandra <sandramoroz@yandex.ru>
2019-09-19 23:23:12 +0300

    step 6

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,3 +1,32 @@
-const game = (number1, operation, number2) => {
-    
-}
+/* eslint-disable default-case */
+import readlineSync from 'readline-sync';
+import { getRandomInt, compareAnswers } from '..';
+
+const getExpressionResult = (number1, operation, number2) => {
+  switch (operation) {
+    case '+':
+      return (number1 + number2);
+    case '*':
+      return (number1 * number2);
+    case '-':
+      return (number1 - number2);
+  }
+  return null;
+};
+
+export default () => {
+  const number1 = getRandomInt(0, 10);
+  const number2 = getRandomInt(0, 10);
+
+  const operators = '+*-';
+
+  const randomOperatorNumber = getRandomInt(0, 2);
+  const randomOperatorChosen = operators[randomOperatorNumber];
+
+  console.log(`Question: ${number1} ${randomOperatorChosen} ${number2}`);
+
+  const expectedAnswer = getExpressionResult(number1, randomOperatorChosen, number2);
+  const receivedAnswer = readlineSync.question('Your answer: ');
+
+  return compareAnswers(expectedAnswer, Number(receivedAnswer));
+};
