4fe1d6bf536b1e7b6dc58da6cb119c1929d25066
Sandra <sandramoroz@yandex.ru>
2019-10-13 12:56:36 +0300

    refactoring

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,6 +3,8 @@ import { cons } from '@hexlet/pairs';
 import getRandomNum from '../utils';
 import game from '..';
 
+const operators = '+*-';
+
 const getExpressionResult = (number1, operation, number2) => {
   switch (operation) {
     case '+':
@@ -15,19 +17,17 @@ const getExpressionResult = (number1, operation, number2) => {
   return null;
 };
 
-const operators = '+*-';
-
 const gameDescription = 'What is the result of the expression?';
 
 const generateGameData = () => {
   const number1 = getRandomNum(0, 10);
   const number2 = getRandomNum(0, 10);
 
-  const randomOperator = operators[getRandomNum(0, operators.length - 1)];
+  const operator = operators[getRandomNum(0, operators.length - 1)];
 
-  const question = `${number1} ${randomOperator} ${number2}`;
+  const question = `${number1} ${operator} ${number2}`;
 
-  const expectedAnswer = getExpressionResult(number1, randomOperator, number2);
+  const expectedAnswer = getExpressionResult(number1, operator, number2);
 
   return cons(question, expectedAnswer);
 };
